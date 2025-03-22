0b89145cb4bae6a05706cbe54178e33361cbb8cd
aeen bolandian <bolandian85@gmail.com>
2025-03-22 16:10:59 +0330

    knight and armors completed

--- Java-Ring/src/main/java/org/project/object/armors/Armor.java
+++ Java-Ring/src/main/java/org/project/object/armors/Armor.java
@@ -1,5 +1,6 @@
 package org.project.object.armors;
 
+import org.project.entity.Entity;
 import org.project.object.Object;
 
 import java.io.Serializable;
@@ -7,15 +8,18 @@ import java.io.Serializable;
 // TODO: UPDATE IMPLEMENTATION
 public abstract class Armor implements Object {
     private int defense;
-    private int maxDefense;
+    private final int maxDefense;
     private int durability;
-    private int maxDurability;
+    private final int maxDurability;
 
     private boolean isBroke;
 
     public Armor(int defense, int durability) {
         this.defense = defense;
         this.durability = durability;
+        this.maxDefense = defense;
+        this.maxDurability = durability;
+        this.isBroke = false;
     }
 
     public void checkBreak() {
@@ -26,6 +30,7 @@ public abstract class Armor implements Object {
     }
 
     // TODO: (BONUS) UPDATE THE REPAIR METHOD
+    // check the repair condition (mana) on main method
     public void repair() {
         isBroke = false;
         defense = maxDefense;
@@ -35,12 +40,17 @@ public abstract class Armor implements Object {
     public int getDefense() {
         return defense;
     }
+    public void setDefense(int defense) {this.defense = defense;}
 
     public int getDurability() {
         return durability;
     }
+    public void setDurability(int durability) {this.durability = durability;}
 
     public boolean isBroke() {
         return isBroke;
+    }
+    public void setBroke(boolean broke) {
+        isBroke = broke;
     }
 }
--- Java-Ring/src/main/java/org/project/object/armors/KnightArmor.java
+++ Java-Ring/src/main/java/org/project/object/armors/KnightArmor.java
@@ -4,13 +4,27 @@ import org.project.entity.Entity;
 
 // TODO: UPDATE IMPLEMENTATION
 public class KnightArmor extends Armor {
+
+    private final int repairCost;
+    private int damageReduction;
     public KnightArmor(int defense, int durability) {
         super(defense, durability);
+        repairCost = 10;
+        damageReduction = 5;
+    }
+
+    public int getRepairCost() {
+        return repairCost;
     }
 
     @Override
     public void use(Entity target) {
-
+        if(damageReduction > 0) {
+            setDefense(getDefense() + damageReduction);
+            damageReduction -= 1;
+        }
+        else
+            setBroke(true);
     }
     // TODO: DESIGN ARMOR'S ATTRIBUTES IMPLEMENT THE CONSTRUCTOR
 }
