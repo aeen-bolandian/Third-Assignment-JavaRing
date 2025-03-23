330181ff7d6c2ab8a77ddab87d79a7f57ff7510a
aeen bolandian <bolandian85@gmail.com>
2025-03-23 17:31:40 +0330

    Magic Shield class added

---
+++ Java-Ring/src/main/java/org/project/object/armors/MagicShield.java
@@ -0,0 +1,27 @@
+package org.project.object.armors;
+
+import org.project.entity.Entity;
+import org.project.entity.players.Wizard;
+
+public class MagicShield extends Armor {
+    public MagicShield(int maxDefense, int repairCost) {
+        super(maxDefense, repairCost);
+    }
+
+    @Override
+    public void use(Entity target) {
+        if(target instanceof Wizard wizard) {
+            if(!isBroke() && isActive()) {
+                wizard.setDefense(wizard.getDefense() + getDefense());
+                setDefense(getDefense() - 1);
+            }
+            else if(isBroke()) {
+                setBroke(true);
+                System.out.println("Magic Shield has been broken");
+            }
+            else if(!isActive()) {
+                System.out.println("Magic Shield is not active");
+            }
+        }
+    }
+}
